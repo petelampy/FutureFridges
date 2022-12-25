84c3856038569d8454c6e456fadcbedef440c6fe
Peter Lampard <peter.lampard@pinewood.co.uk>
2022-12-25 23:46:17 +0000

    Added permissions based access to Stock Viewing/Removal

--- Pages/StockManagement/StockManagement.cshtml.cs
+++ Pages/StockManagement/StockManagement.cshtml.cs
@@ -1,31 +1,41 @@
 using FutureFridges.Business.StockManagement;
+using FutureFridges.Business.UserManagement;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace FutureFridges.Pages.StockManagement
 {
+    [Authorize]
     public class StockManagementModel : PageModel
     {
+        private const string ACCESS_ERROR_PAGE_PATH = "../Account/AccessError";
+
         private readonly ProductController __ProductController;
         private readonly StockItemController __StockItemController;
+        private readonly UserPermissionController __UserPermissionController;
 
         public StockManagementModel ()
         {
             __StockItemController = new StockItemController();
             __ProductController = new ProductController();
+            __UserPermissionController = new UserPermissionController();
         }
 
-        public void OnGet ()
+        public IActionResult OnGet ()
         {
-            StockItems = __StockItemController.GetAll();
-            Products = __ProductController.GetAll();
-        }
+            SetUserPermissions();
 
-        public void OnPostSelectProduct (Guid uid)
-        {
-            StockItems = __StockItemController.GetAll();
-            Products = __ProductController.GetAll();
-            SelectedProduct = __ProductController.GetProduct(uid);
+            if (CurrentUserPermissions.ViewStock)
+            {
+                SetStockAndProducts();
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage(ACCESS_ERROR_PAGE_PATH);
+            }
         }
 
         public async Task<IActionResult> OnGetTakeProduct (Guid uid)
@@ -33,8 +43,8 @@ namespace FutureFridges.Pages.StockManagement
             StockItem _CurrentItem = __StockItemController.GetStockItem(uid);
             __StockItemController.DeleteStockItem(uid);
 
-            StockItems = __StockItemController.GetAll();
-            Products = __ProductController.GetAll();
+            SetStockAndProducts();
+            SetUserPermissions();
 
             int _RemainingQuantity = StockItems.Where(stockItem => stockItem.Product_UID == _CurrentItem.Product_UID).Count();
             if (_RemainingQuantity > 0)
@@ -48,9 +58,30 @@ namespace FutureFridges.Pages.StockManagement
             }
         }
 
+        public void OnPostSelectProduct (Guid uid)
+        {
+            SetUserPermissions();
+            SetStockAndProducts();
+
+            SelectedProduct = __ProductController.GetProduct(uid);
+        }
+
+        public void SetStockAndProducts ()
+        {
+            StockItems = __StockItemController.GetAll();
+            Products = __ProductController.GetAll();
+        }
+
+        public void SetUserPermissions ()
+        {
+            string _CurrentUserID = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            CurrentUserPermissions = __UserPermissionController.GetPermissions(new Guid(_CurrentUserID));
+        }
+
+        public UserPermissions CurrentUserPermissions { get; set; }
+        public string ProductImagePath { get; set; } = "../Images/Products/";
         public List<Product> Products { get; set; }
         public Product? SelectedProduct { get; set; } = new Product();
         public List<StockItem> StockItems { get; set; }
-        public string ProductImagePath { get; set; } = "../Images/Products/";
     }
 }
