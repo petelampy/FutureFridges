4a3417c1331c95b404b635abee568a86aea3f3cb
N0923375 <N0923375@my.ntu.ac.uk>
2023-01-04 22:57:16 +0000

    Added Automated Tests project
    Currently just 1 passing test which writes "Hello world!" to the console.

---
+++ AutomatedTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace AutomatedTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1 ()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
