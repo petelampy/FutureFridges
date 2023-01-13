a02b908bf8255356bd89ec6a75c1daa3ea4effd5
Jak Waterfall <N0906911@my.ntu.ac.uk>
2023-01-13 08:07:43 +0000

    Migrated DeliveryDriver login to Login Page. Changed validation and Post functionality to support multiple Post functions in one page.

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,3 +1,4 @@
+using FutureFridges.Business.OrderManagement;
 using FutureFridges.Business.UserManagement;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -12,21 +13,24 @@ namespace FutureFridges.Pages.Account
 
         private readonly SignInManager<FridgeUser> __SignInManager;
 
+        private readonly IOrderController __OrderController;
+
         public LoginModel (SignInManager<FridgeUser> signInManager)
         {
             __SignInManager = signInManager;
+            __OrderController = new OrderController();
         }
 
         public void OnGetAsync ()
         {
         }
 
-        public async Task<IActionResult> OnPostAsync (string? returnUrl = null)
+        public async Task<IActionResult> OnPostLoginAsync (string username, string password, bool rememberMe, string? returnUrl = null)
         {
             if (ModelState.IsValid)
             {
                 Microsoft.AspNetCore.Identity.SignInResult _IdentityResult = await __SignInManager
-                    .PasswordSignInAsync(Username, Password, RememberMe, false);
+                    .PasswordSignInAsync(username, password, rememberMe, false);
 
                 if (_IdentityResult.Succeeded)
                 {
@@ -41,23 +45,24 @@ namespace FutureFridges.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Incorrect username or password!");
+                    ModelState.AddModelError("LoginError", "Incorrect Username or Password!");
                 }
             }
 
             return Page();
         }
 
-        [BindProperty]
-        [DataType(DataType.Password)]
-        [Required(ErrorMessage = "Please enter a password!")]
-        public string Password { get; set; }
-
-        [BindProperty]
-        public bool RememberMe { get; set; }
-
-        [BindProperty]
-        [Required(ErrorMessage = "Please enter a username!")]
-        public string Username { get; set; }
+        public IActionResult OnPostPinCodeLogin (int pinCode)
+        {
+            if (!__OrderController.IsValidOrderPinCode(pinCode))
+            {
+                ModelState.AddModelError("PinError", "Invalid Pin Code");
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("../Deliveries/ReviewDelivery", new { PinCode = pinCode });
+            }
+        }
     }
 }
