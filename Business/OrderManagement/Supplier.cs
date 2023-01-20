1b698d448b1e2f20898737228a41bd1a669a4b0b
Jak Waterfall <N0906911@my.ntu.ac.uk>
2023-01-20 09:42:53 +0000

    Changed Suppliers Name and Email to be nullable to fix bug where database crashes if no name or email is given in the create supplier Page

--- Business/OrderManagement/Supplier.cs
+++ Business/OrderManagement/Supplier.cs
@@ -2,9 +2,9 @@
 {
     public class Supplier
     {
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public Guid UID { get; set; }
     }
 }
