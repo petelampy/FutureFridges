2131207c99a3625bce5c74be9e93726b17a19b04
Peter Lampard <peter.lampard@pinewood.co.uk>
2023-02-02 18:21:35 +0000

    Audit log page adapted to work for both head chefs and administrators

--- Pages/AuditLog/AuditLog.cshtml.cs
+++ Pages/AuditLog/AuditLog.cshtml.cs
@@ -1,6 +1,10 @@
 using FutureFridges.Business.AuditLog;
+using FutureFridges.Business.Enums;
+using FutureFridges.Business.UserManagement;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace FutureFridges.Pages.Logs
 {
@@ -8,21 +12,33 @@ namespace FutureFridges.Pages.Logs
     {
 
         private readonly IAuditLogController __AuditLogController;
+        private readonly IUserController __UserController;
 
-        public AuditLogModel ()
+        public AuditLogModel (UserManager<FridgeUser> userManager)
         {
             __AuditLogController = new AuditLogController();
+            __UserController = new UserController(userManager);
         }
 
         public IActionResult OnGet ()
         {
-            //Get current user account, if admin show all logs, if head chef show stock logs
-            //Logs = __AuditLogController.GetStockLogs();
-            Logs = __AuditLogController.GetAll();
+            string _CurrentUserID = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            CurrentUser = __UserController.GetUser(_CurrentUserID);
+
+            if(CurrentUser.UserType == UserType.Administrator)
+            {
+                Logs = __AuditLogController.GetAll();
+            }
+            else
+            {
+                Logs = __AuditLogController.GetStockLogs();
+            }
 
             return Page();
         }
 
         public List<LogEntry> Logs { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public FridgeUser CurrentUser { get; set; }
     }
 }
