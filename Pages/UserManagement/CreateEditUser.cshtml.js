bbaf9167dd9018ab47e533016f0dd9fb1e314509
Peter Lampard <34271471+petelampy@users.noreply.github.com>
2023-02-08 16:13:59 +0000

    JS to hide/unhide checkboxes based on view stock permission

---
+++ Pages/UserManagement/CreateEditUser.cshtml.js
@@ -0,0 +1,14 @@
+﻿const AddRemovePermissions = document.getElementById("addRemoveStockPermissions");
+const ViewStockCheckbox = document.getElementById("viewStockCheckbox");
+
+function showHideCheckboxes() {
+    if (ViewStockCheckbox.checked) {
+        AddRemovePermissions.style.display = "block";
+    } else {
+        AddRemovePermissions.style.display = "none";
+    }
+}
+
+ViewStockCheckbox.addEventListener('change', showHideCheckboxes);
+
+window.onload = function () { showHideCheckboxes() };
